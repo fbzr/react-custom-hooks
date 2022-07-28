5538db8296223348c8959e498b37527832408b07
Fabricio Bezerra <fabricio.spb@gmail.com>
2022-07-28 10:10:51 -0300

    update App compoent

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,23 @@
-import { useState } from 'react';
-import { useWindowSize } from './hooks/useWindowSize';
 import WindowSize from './components/WindowSize';
 import ResizableContainer from './components/ResizableContainer';
 import List from './components/List';
-import './App.css';
+import ListPaginated from './components/ListPaginated';
 import CurrentBreakpoint from './components/CurrentBreakpoint';
+import ListAllFeatures from './components/ListAllFeatures';
+
+import './App.css';
+import LocalStorageInput from './components/LocalStorageInput';
 
 function App() {
   return (
     <div className="App">
       <WindowSize />
       <CurrentBreakpoint />
-
       <ResizableContainer />
-
       <List />
+      <ListAllFeatures />
+      <ListPaginated />
+      <LocalStorageInput />
     </div>
   );
 }
